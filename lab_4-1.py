2e423ec5ea2d097350151fca8761d0bad1f26631
Cam Petrelli <p22cpetrelli@fairfieldprep.org>
2021-10-26 10:11:43 -0400

    add file

---
+++ lab_4-1.py
@@ -0,0 +1,8 @@
+# author CJP 10/21/2021
+
+print("Fairfield" + " Prep")
+print("-" * 20)
+print("I'm loving this short story I'm reading, 'The Fall of the House of Usher'")
+print(len("I'm loving this short story I'm reading, 'The Fall of the House of Usher'"))
+my_string = "apple"
+print(my_string[3])
