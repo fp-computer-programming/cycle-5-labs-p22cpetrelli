0e39d3a66d3ebcf249bc487fc40399c3e32b3ebb
Cam Petrelli <p22cpetrelli@fairfieldprep.org>
2021-10-29 10:41:14 -0400

    add file

---
+++ lab_6-1.py
@@ -0,0 +1,6 @@
+# author CJP 10/21/2021
+word = "flibbertigibbet"
+print(word.find("t"))
+
+ss = "I wish, I wish, I was a fish."
+print(ss.split(","))
---
+++ lab_6-2.py
@@ -0,0 +1,5 @@
+# author CJP 10/21/2021
+person1 = input("What is the frst persons name? ")
+person2 = input("What is the second persons name? ")
+fs = "Hello, {1}. My name is {0}.".format(person1, person2)
+print(fs)
